a34ab62fe077c0fa9c5f37510761eaf66dc039f8
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2025-01-17 20:09:31 +0530

    fourth  update

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -5,10 +5,10 @@ const AboutMe = () => {
     <section className='sm:w-[60%] w-[90%] sm:p-3 p-1 mt-4'>
       <div>
         <h1 className='font-semibold text-lg'>About Me</h1>
-        <p className='sm:text-[0.8rem] text-[0.6rem] mt-2'>
+        <p className='sm:text-lg text-sm mt-2'>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus quae eos ipsa porro voluptatum corporis magni, perspiciatis voluptas fuga sint.
         </p>
-        <p className='sm:text-[0.8rem] text-[0.6rem] mt-4'>
+        <p className='sm:text-lg text-sm mt-4'>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus quae eos ipsa porro voluptatum corporis magni, perspiciatis voluptas fuga sint.
         </p>
       </div>
